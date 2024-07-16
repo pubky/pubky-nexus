e142879e33dad03c5cd89abf769a6abccf92daf1
SHAcollision <cotyleron@proton.me>
2024-07-16 16:30:38 +0200

    fix(db): improve example write speed

--- neo4j-example/run-queries.sh
+++ neo4j-example/run-queries.sh
@@ -4,9 +4,4 @@
 QUERIES_FILE="/neo4j-example/indexer-migration.cypher"
 
 # Read queries from the file and execute each one
-while IFS= read -r query
-do
-    if [[ ! -z "$query" ]]; then
-        cypher-shell -u neo4j -p 12345678 "$query"
-    fi
-done < "$QUERIES_FILE"
+cypher-shell -u neo4j -p 12345678 -f "$QUERIES_FILE"
