2d9be32ee0a95334a282aaaaacfadd2bd71ee6c7
SHAcollision <cotyleron@proton.me>
2024-07-18 02:21:57 +0200

    feat: refactor configs

--- service/src/routes/static/mod.rs
+++ service/src/routes/static/mod.rs
@@ -1,8 +1,6 @@
 use axum::{routing::get_service, Router};
-use std::env;
 use tower_http::services::ServeDir;
 
-pub fn routes() -> Router {
-    let static_path = env::var("STATIC_PATH").expect("STATIC_PATH environment variable not set");
+pub fn routes(static_path: &String) -> Router {
     Router::new().nest_service("/static", get_service(ServeDir::new(static_path)))
 }
