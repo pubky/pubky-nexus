5050254f1f043ba6e5d7efeea2be1891d985a885
SHAcollision <cotyleron@proton.me>
2024-07-21 01:49:18 +0200

    Reuse route prefixes

--- src/routes/v0/endpoints.rs
+++ src/routes/v0/endpoints.rs
@@ -1,10 +1,12 @@
+use const_format::concatcp;
+
 // Info routes
 pub const INFO_ROUTE: &str = "/v0/info";
 
 // Profile routes
-// TODO: try using route prefixes: pub const PROFILE_ROUTE: &str = "/v0/profile";
-pub const PROFILE_ROUTE: &str = "/v0/profile/:user_id";
-pub const RELATIONSHIP_ROUTE: &str = "/v0/profile/:user_id/relationship/:viewer_id";
-pub const PROFILE_COUNTS_ROUTE: &str = "/v0/profile/:user_id/counts";
-pub const PROFILE_DETAILS_ROUTE: &str = "/v0/profile/:user_id/details";
-pub const PROFILE_TAGS_ROUTE: &str = "/v0/profile/:user_id/tags";
+const PROFILE_PREFIX: &str = "/v0/profile";
+pub const PROFILE_ROUTE: &str = concatcp!(PROFILE_PREFIX, "/:user_id");
+pub const RELATIONSHIP_ROUTE: &str = concatcp!(PROFILE_PREFIX, "/:user_id/relationship/:viewer_id");
+pub const PROFILE_COUNTS_ROUTE: &str = concatcp!(PROFILE_PREFIX, "/:user_id/counts");
+pub const PROFILE_DETAILS_ROUTE: &str = concatcp!(PROFILE_PREFIX, "/:user_id/details");
+pub const PROFILE_TAGS_ROUTE: &str = concatcp!(PROFILE_PREFIX, "/:user_id/tags");
