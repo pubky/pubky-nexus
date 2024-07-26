04453d9ba8632b89219dc139968457ceb7e4fab0
SHAcollision <cotyleron@proton.me>
2024-07-26 12:50:57 +0200

    Feat combine json/bool set and enable JSON set_mul get_range

--- src/db/kv/index.rs
+++ src/db/kv/index.rs
@@ -1,18 +1,21 @@
 use crate::db::connectors::redis::get_redis_conn;
-use log::debug;
+use log::{debug, error};
 use redis::{AsyncCommands, JsonAsyncCommands};
 use serde::de::DeserializeOwned;
 use serde::Serialize;
 use std::error::Error;
 
-/// Sets a JSON value in Redis at a specified path with an optional expiration time.
+/// Sets a value in Redis, supporting both JSON objects and boolean values.
+///
+/// This function stores JSON objects using RedisJSON and booleans as integers (0 or 1).
+/// An optional expiration time can be set for both types.
 ///
 /// # Arguments
 ///
-/// * `prefix` - A string slice that represents the prefix for the Redis key.
-/// * `key` - A string slice that represents the key under which the value is stored.
-/// * `value` - A reference to the value to be stored, which must implement `Serialize`, `Send`, and `Sync`.
-/// * `path` - An optional string slice representing the JSON path where the value should be set. Defaults to the root path "$".
+/// * `prefix` - A string slice representing the prefix for the Redis key.
+/// * `key` - A string slice representing the key under which the value is stored.
+/// * `value` - A reference to the value to be stored. If the value is a boolean, it will be stored as 0 or 1. For other types, it must implement `Serialize`.
+/// * `path` - An optional string slice representing the JSON path where the value should be set for JSON objects. Defaults to the root path "$".
 /// * `expiration` - An optional expiration time in seconds. If provided, the key will expire after this duration.
 ///
 /// # Errors
@@ -24,21 +27,54 @@ pub async fn set<T: Serialize + Send + Sync>(
     value: &T,
     path: Option<&str>,
     expiration: Option<i64>,
-) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let mut redis_conn = get_redis_conn().await?;
+) -> Result<(), Box<dyn Error + Send + Sync>> {
     let index_key = format!("{}:{}", prefix, key);
-    let json_path = path.unwrap_or("$");
 
-    // Use RedisJSON commands to set the value at the specified path
-    redis_conn.json_set(&index_key, json_path, value).await?;
-
-    if let Some(exp) = expiration {
-        redis_conn.expire(&index_key, exp).await?;
-        debug!("Indexed key: {} with expiration: {}", index_key, exp);
-    } else {
-        debug!("Indexed key: {} with no expiration", index_key);
+    match serde_json::to_value(value)? {
+        serde_json::Value::Bool(boolean_value) => {
+            set_boolean(&index_key, boolean_value, expiration).await?;
+        }
+        _ => {
+            set_json(&index_key, value, path, expiration).await?;
+        }
     }
 
+    debug!(
+        "Set key: {} with optional expiration: {:?}",
+        index_key, expiration
+    );
+    Ok(())
+}
+
+async fn set_boolean(
+    key: &str,
+    value: bool,
+    expiration: Option<i64>,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    let mut redis_conn = get_redis_conn().await?;
+
+    let int_value = if value { 1 } else { 0 };
+    if let Some(exp) = expiration {
+        redis_conn.set_ex(key, int_value, exp as u64).await?;
+    } else {
+        redis_conn.set(key, int_value).await?;
+    }
+    Ok(())
+}
+
+async fn set_json<T: Serialize + Send + Sync>(
+    key: &str,
+    value: &T,
+    path: Option<&str>,
+    expiration: Option<i64>,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    let mut redis_conn = get_redis_conn().await?;
+
+    let json_path = path.unwrap_or("$");
+    redis_conn.json_set(key, json_path, value).await?;
+    if let Some(exp) = expiration {
+        redis_conn.expire(key, exp).await?;
+    }
     Ok(())
 }
 
@@ -77,46 +113,6 @@ pub async fn get<T: DeserializeOwned + Send + Sync>(
     }
 
     Ok(None)
-}
-
-/// Sets a boolean value in Redis with an optional expiration time.
-///
-/// # Arguments
-///
-/// * `prefix` - A string slice that represents the prefix for the Redis key.
-/// * `key` - A string slice that represents the key under which the value is stored.
-/// * `value` - A boolean value to be stored.
-/// * `expiration` - An optional expiration time in seconds. If provided, the key will expire after this duration.
-///
-/// # Errors
-///
-/// Returns an error if the operation fails.
-pub async fn _set_bool(
-    prefix: &str,
-    key: &str,
-    value: bool,
-    expiration: Option<u64>,
-) -> Result<(), Box<dyn Error + Send + Sync>> {
-    let mut redis_conn = get_redis_conn().await?;
-    let index_key = format!("{}:{}", prefix, key);
-
-    let int_value = if value { 1 } else { 0 };
-
-    match expiration {
-        Some(exp) => {
-            redis_conn.set_ex(&index_key, int_value, exp).await?;
-            debug!(
-                "Indexed boolean key: {} with expiration: {}",
-                index_key, exp
-            );
-        }
-        None => {
-            redis_conn.set(&index_key, int_value).await?;
-            debug!("Indexed boolean key: {} with no expiration", index_key);
-        }
-    }
-
-    Ok(())
 }
 
 /// Retrieves a boolean value from Redis.
@@ -199,12 +195,23 @@ pub async fn get_range<T: DeserializeOwned + Send + Sync>(
     drop(iter);
 
     let selected_keys: Vec<String> = keys_to_get.into_iter().skip(skip).take(limit).collect();
-    let values: Vec<String> = redis_conn.mget(selected_keys).await?;
 
-    let mut results = vec![];
-    for value in values {
-        let deserialized_value: T = serde_json::from_str(&value)?;
-        results.push(deserialized_value);
+    // Using a pipeline to get multiple JSON values
+    let mut pipeline = redis::pipe();
+    for key in &selected_keys {
+        pipeline.cmd("JSON.GET").arg(key).arg("$");
+    }
+
+    let json_values: Vec<Option<String>> = pipeline.query_async(&mut redis_conn).await?;
+
+    let mut results = Vec::with_capacity(json_values.len());
+    for json_value in json_values {
+        if let Some(json_value) = json_value {
+            let mut deserialized_values: Vec<T> = serde_json::from_str(&json_value)?;
+            if let Some(value) = deserialized_values.pop() {
+                results.push(value);
+            }
+        }
     }
 
     Ok(results)
@@ -259,8 +266,7 @@ pub async fn set_multiple<T: Serialize>(
 
     for (key, value) in data {
         let full_key = format!("{}{}", prefix, key.as_ref());
-        let json_value = serde_json::to_string(&value)?;
-        cmd.set(&full_key, json_value);
+        cmd.json_set(&full_key, "$", value)?;
     }
 
     cmd.query_async(&mut redis_conn).await?;
