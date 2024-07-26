4a74879b47d75737bf791bf0edf8749bd5926890
SHAcollision <cotyleron@proton.me>
2024-07-26 13:17:42 +0200

    Add get_bool_range

--- src/db/kv/index.rs
+++ src/db/kv/index.rs
@@ -152,6 +152,75 @@ pub async fn _get_bool(
     Ok(None)
 }
 
+/// Sets a list of keys and their corresponding values in Redis in a single operation.
+///
+/// This function handles both JSON and boolean values, using RedisJSON for JSON data and storing booleans
+/// as integers. It uses multiple commands in a single request to set key-value pairs efficiently.
+/// Each value must implement the `Serialize` trait for JSON or be a boolean.
+///
+/// # Arguments
+///
+/// * `prefix` - A string slice representing the prefix for the Redis keys.
+/// * `data` - A slice of tuples where each tuple contains a key as a string slice and a value that implements `Serialize`.
+///
+/// # Returns
+///
+/// Returns a `Result` indicating success or an error if the operation fails.
+///
+/// # Errors
+///
+/// This function will return an error if there are issues connecting to Redis, or if serialization of the values fails.
+///
+/// # Examples
+///
+/// ```ignore
+/// use serde::Serialize;
+///
+/// #[derive(Serialize)]
+/// struct MyValue {
+///     field1: String,
+///     field2: i32,
+/// }
+///
+/// let data = vec![
+///     ("key1", MyValue { field1: "value1".to_string(), field2: 10 }),
+///     ("key2", MyValue { field1: "value2".to_string(), field2: 20 }),
+///     ("key3", true), // boolean value
+/// ];
+///
+/// set_multiple::<MyValue>("prefix:", &data).await?;
+/// ```
+///
+/// This example sets multiple key-value pairs with a common prefix in Redis.
+pub async fn set_multiple<T: Serialize>(
+    prefix: &str,
+    data: &[(impl AsRef<str>, T)],
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    let mut redis_conn = get_redis_conn().await?;
+
+    // Create a pipeline-like command sequence
+    let mut cmd = redis::pipe();
+
+    for (key, value) in data {
+        let full_key = format!("{}{}", prefix, key.as_ref());
+
+        // Check if the value is boolean
+        match serde_json::to_value(value)? {
+            serde_json::Value::Bool(boolean_value) => {
+                let int_value = if boolean_value { 1 } else { 0 };
+                cmd.set(&full_key, int_value);
+            }
+            _ => {
+                // Handle other values as JSON
+                cmd.json_set(&full_key, "$", value)?;
+            }
+        }
+    }
+
+    cmd.query_async(&mut redis_conn).await?;
+    Ok(())
+}
+
 /// Retrieves a range of values from Redis based on a pattern, with optional skip and limit for pagination.
 ///
 /// # Arguments
@@ -217,60 +286,60 @@ pub async fn get_range<T: DeserializeOwned + Send + Sync>(
     Ok(results)
 }
 
-/// Sets a list of keys and their corresponding JSON values in Redis in a single operation.
-///
-/// This function uses multiple `SET` commands in a single request to set key-value pairs efficiently.
-/// Each value must implement the `Serialize` trait to be converted into JSON format before storing.
+/// Retrieves a range of boolean values from Redis based on a pattern, with optional skip and limit for pagination.
 ///
 /// # Arguments
 ///
-/// * `prefix` - A string slice representing the prefix for the Redis keys.
-/// * `data` - A slice of tuples where each tuple contains a key as a string slice and a value that implements `Serialize`.
+/// * `prefix` - A string slice that represents the prefix for the Redis key.
+/// * `pattern` - An optional string slice representing the pattern to match keys.
+/// * `skip` - An optional number of keys to skip (for pagination).
+/// * `limit` - An optional number of keys to return (for pagination).
 ///
 /// # Returns
 ///
-/// Returns a `Result` indicating success or an error if the operation fails.
+/// Returns a vector of boolean values if they exist, or an empty vector if no matching keys are found.
 ///
 /// # Errors
 ///
-/// This function will return an error if there are issues connecting to Redis, or if serialization of the values fails.
-///
-/// # Examples
-///
-/// ```ignore
-/// use serde::Serialize;
-///
-/// #[derive(Serialize)]
-/// struct MyValue {
-///     field1: String,
-///     field2: i32,
-/// }
-///
-/// let data = vec![
-///     ("key1", MyValue { field1: "value1".to_string(), field2: 10 }),
-///     ("key2", MyValue { field1: "value2".to_string(), field2: 20 }),
-/// ];
-///
-/// set_multiple::<MyValue>("prefix:", &data).await?;
-/// ```
-///
-/// This example sets multiple key-value pairs with a common prefix in Redis.
-pub async fn set_multiple<T: Serialize>(
+/// Returns an error if the operation fails.
+pub async fn get_bool_range(
     prefix: &str,
-    data: &[(impl AsRef<str>, T)],
-) -> Result<(), Box<dyn Error + Send + Sync>> {
+    pattern: Option<&str>,
+    skip: Option<usize>,
+    limit: Option<usize>,
+) -> Result<Vec<bool>, Box<dyn Error + Send + Sync>> {
+    let pattern = pattern.unwrap_or("*");
     let mut redis_conn = get_redis_conn().await?;
+    let mut iter = redis_conn
+        .scan_match::<String, String>(format!("{}{}", prefix, pattern))
+        .await?;
 
-    // Create a pipeline-like command sequence
-    let mut cmd = redis::pipe();
-
-    for (key, value) in data {
-        let full_key = format!("{}{}", prefix, key.as_ref());
-        cmd.json_set(&full_key, "$", value)?;
+    let mut keys_to_get = vec![];
+    while let Some(key) = iter.next_item().await {
+        keys_to_get.push(key);
     }
 
-    cmd.query_async(&mut redis_conn).await?;
-    Ok(())
+    // Sort keys alphanumerically
+    keys_to_get.sort();
+
+    let skip = skip.unwrap_or(0);
+    let limit = limit.unwrap_or(keys_to_get.len());
+
+    // Drop the iterator to release the mutable borrow on redis_conn
+    drop(iter);
+
+    let selected_keys: Vec<String> = keys_to_get.into_iter().skip(skip).take(limit).collect();
+
+    // Fetch the values using a single command
+    let values: Vec<Option<i32>> = redis_conn.mget(&selected_keys).await?;
+
+    let results: Vec<bool> = values
+        .into_iter()
+        .filter_map(|opt| opt)
+        .map(|val| val != 0)
+        .collect();
+
+    Ok(results)
 }
 
 #[cfg(test)]
@@ -286,43 +355,63 @@ mod tests {
         field2: i32,
     }
 
+    // #[tokio::test]
+    // async fn test_get_range() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    //     let config = Config::from_env();
+    //     setup(&config).await;
+
+    //     let data = vec![
+    //         (
+    //             "key1",
+    //             MyValue {
+    //                 field1: "value1".to_string(),
+    //                 field2: 10,
+    //             },
+    //         ),
+    //         (
+    //             "key2",
+    //             MyValue {
+    //                 field1: "value2".to_string(),
+    //                 field2: 20,
+    //             },
+    //         ),
+    //         (
+    //             "key3",
+    //             MyValue {
+    //                 field1: "value3".to_string(),
+    //                 field2: 30,
+    //             },
+    //         ),
+    //     ];
+
+    //     // Set values in Redis
+    //     set_multiple::<MyValue>("test:", &data).await?;
+
+    //     // Retrieve values using `get_range` with a specific pattern
+    //     let result = get_range::<MyValue>("test:", Some("key*"), Some(0), Some(10)).await?;
+    //     assert_eq!(result.len(), data.len());
+
+    //     let expected_values: Vec<MyValue> = data.into_iter().map(|(_, v)| v).collect();
+    //     assert_eq!(result, expected_values);
+
+    //     Ok(())
+    // }
+
     #[tokio::test]
-    async fn test_get_range() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    async fn test_get_bool_range() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         let config = Config::from_env();
         setup(&config).await;
 
-        let data = vec![
-            (
-                "key1",
-                MyValue {
-                    field1: "value1".to_string(),
-                    field2: 10,
-                },
-            ),
-            (
-                "key2",
-                MyValue {
-                    field1: "value2".to_string(),
-                    field2: 20,
-                },
-            ),
-            (
-                "key3",
-                MyValue {
-                    field1: "value3".to_string(),
-                    field2: 30,
-                },
-            ),
-        ];
+        let data = vec![("bool1", true), ("bool2", false), ("bool3", true)];
 
-        // Set values in Redis
-        set_multiple::<MyValue>("test:", &data).await?;
+        // Set boolean values in Redis
+        set_multiple::<bool>("test:", &data).await?;
 
-        // Retrieve values using `get_range` with a specific pattern
-        let result = get_range::<MyValue>("test:", Some("key*"), Some(0), Some(10)).await?;
+        // Retrieve boolean values using `get_bool_range` with a specific pattern
+        let result = get_bool_range("test:", Some("bool*"), Some(0), Some(10)).await?;
         assert_eq!(result.len(), data.len());
 
-        let expected_values: Vec<MyValue> = data.into_iter().map(|(_, v)| v).collect();
+        let expected_values: Vec<bool> = data.into_iter().map(|(_, v)| v).collect();
         assert_eq!(result, expected_values);
 
         Ok(())
