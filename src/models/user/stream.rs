576c3181d888cfa5f33fb8c2be89f661e954f35e
SHAcollision <cotyleron@proton.me>
2024-07-30 18:41:59 +0200

    Add concurrent stream creation and viewer_id

--- src/models/user/stream.rs
+++ src/models/user/stream.rs
@@ -2,6 +2,7 @@ use std::error::Error;
 
 use super::{Followers, Following, UserView};
 use serde::{Deserialize, Serialize};
+use tokio::task::spawn;
 use utoipa::ToSchema;
 
 #[derive(Deserialize, ToSchema)]
@@ -26,6 +27,7 @@ impl UserStream {
 
     pub async fn get_by_id(
         user_id: &str,
+        viewer_id: Option<&str>,
         skip: Option<usize>,
         limit: Option<usize>,
         list_type: UserStreamType,
@@ -39,20 +41,37 @@ impl UserStream {
                 .map(|following| following.0),
         };
         match user_ids {
-            Some(users) => Self::from_listed_user_ids(&users).await,
+            Some(users) => Self::from_listed_user_ids(&users, viewer_id).await,
             None => Ok(None),
         }
     }
 
     pub async fn from_listed_user_ids(
         user_ids: &[String],
+        viewer_id: Option<&str>,
     ) -> Result<Option<Self>, Box<dyn std::error::Error + Send + Sync>> {
-        let mut user_views = Vec::with_capacity(user_ids.len());
+        // TODO: potentially we could use a new redis_com.mget() with a single call to retrieve all
+        // user details at once and build the user profiles on the fly.
+        // But still, using tokio to create them concurrently has VERY high performance.
+        let viewer_id = viewer_id.map(|id| id.to_string());
+        let mut handles = Vec::with_capacity(user_ids.len());
+
         for user_id in user_ids {
-            if let Some(user_view) = UserView::get_by_id(user_id, None).await? {
+            let user_id = user_id.clone();
+            let viewer_id = viewer_id.clone();
+            let handle =
+                spawn(async move { UserView::get_by_id(&user_id, viewer_id.as_deref()).await });
+            handles.push(handle);
+        }
+
+        let mut user_views = Vec::with_capacity(user_ids.len());
+
+        for handle in handles {
+            if let Some(user_view) = handle.await?? {
                 user_views.push(user_view);
             }
         }
+
         Ok(Some(Self(user_views)))
     }
 }
--- src/routes/v0/stream/users.rs
+++ src/routes/v0/stream/users.rs
@@ -10,6 +10,7 @@ use utoipa::OpenApi;
 #[derive(Deserialize)]
 pub struct UserStreamQuery {
     user_id: Option<String>,
+    viewer_id: Option<String>,
     skip: Option<usize>,
     limit: Option<usize>,
     stream_type: Option<UserStreamType>,
@@ -21,6 +22,7 @@ pub struct UserStreamQuery {
     tag = "Stream Users",
     params(
         ("user_id" = String, Path, description = "User Pubky ID"),
+        ("viewer_id" = Option<String>, Query, description = "Viewer Pubky ID"),
         ("skip" = Option<usize>, Query, description = "Skip N followers"),
         ("limit" = Option<usize>, Query, description = "Retrieve N followers"),
         ("stream_type" = Option<UserStreamType>, Query, description = "Stream Type")
@@ -50,7 +52,15 @@ pub async fn stream_users_handler(
     let limit = query.limit.unwrap_or(20);
     let stream_type = query.stream_type.unwrap_or(UserStreamType::Followers);
 
-    match UserStream::get_by_id(&user_id, Some(skip), Some(limit), stream_type).await {
+    match UserStream::get_by_id(
+        &user_id,
+        query.viewer_id.as_deref(),
+        Some(skip),
+        Some(limit),
+        stream_type,
+    )
+    .await
+    {
         Ok(Some(stream)) => Ok(Json(stream)),
         Ok(None) => Err(Error::UserNotFound { user_id }),
         Err(source) => Err(Error::InternalServerError { source }),
