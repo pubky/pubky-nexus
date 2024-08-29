f8d00e86487407bd2ddded8a145cb98122d07c68
tipogi <tipogi@pm.me>
2024-08-29 19:31:49 +0200

    Add test.cypher to import in the graph

--- docker/db-migration/run-queries.sh
+++ docker/db-migration/run-queries.sh
@@ -2,6 +2,7 @@
 
 # Path to the queries file
 QUERIES_FILE="/db-migration/indexer.cypher"
+TEST_FILE="/db-migration/test.cypher"
 
 echo "Starting Cypher query execution..."
 
@@ -13,6 +14,7 @@ fi
 
 # Execute queries and time the execution
 time cypher-shell -u neo4j -p 12345678 -f "$QUERIES_FILE"
+time cypher-shell -u neo4j -p 12345678 -f "$TEST_FILE"
 
 if [[ $? -eq 0 ]]; then
     echo "Queries executed successfully."
