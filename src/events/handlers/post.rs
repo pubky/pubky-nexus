78f0bd9eeddfa935d27f9d030fc4a87c60ee6deb
SHAcollision <cotyleron@proton.me>
2024-09-02 04:59:38 +0200

    Refactor handlers

---
+++ src/events/handlers/post.rs
@@ -0,0 +1,39 @@
+use crate::models::pubky_app::traits::Validatable;
+use crate::models::{post::PostDetails, pubky_app::PubkyAppPost, user::PubkyId};
+use crate::reindex::reindex_post;
+use axum::body::Bytes;
+use log::debug;
+use std::error::Error;
+
+pub async fn handle_post_put_event(
+    author_id: PubkyId,
+    post_id: String,
+    blob: Bytes,
+) -> Result<(), Box<dyn Error + Sync + Send>> {
+    // Process Post resource and update the databases
+    debug!("Indexing new post: {}/{}", author_id, post_id);
+
+    // Serialize and validate
+    let post = <PubkyAppPost as Validatable>::try_from(&blob)?;
+
+    // Create PostDetails object
+    let post_details = PostDetails::from_homeserver(post, &author_id, &post_id).await?;
+
+    // Add new post node into the graph
+    post_details.put_to_graph().await?;
+
+    // Reindex to sorted sets and other indexes
+    reindex_post(&author_id, &post_id).await?;
+
+    Ok(())
+}
+
+pub async fn handle_post_del_event(
+    author_id: PubkyId,
+    post_id: String,
+) -> Result<(), Box<dyn Error + Sync + Send>> {
+    // TODO: handle deletion of Post resource from databases
+    debug!("Deleting post: {}/{}", author_id, post_id);
+    // Implement logic here
+    Ok(())
+}
---
+++ src/events/handlers/user.rs
@@ -0,0 +1,43 @@
+use crate::models::{
+    pubky_app::{traits::Validatable, PubkyAppUser},
+    traits::Collection,
+    user::{PubkyId, UserCounts, UserDetails},
+};
+use crate::reindex::reindex_user;
+use axum::body::Bytes;
+use log::debug;
+use std::error::Error;
+
+pub async fn handle_user_put_event(
+    user_id: PubkyId,
+    blob: Bytes,
+) -> Result<(), Box<dyn Error + Sync + Send>> {
+    // Process profile.json and update the databases
+    debug!("Indexing new user profile: {}", user_id);
+
+    // Serialize and validate
+    let user = <PubkyAppUser as Validatable>::try_from(&blob)?;
+
+    // Create UserDetails object
+    let user_details = UserDetails::from_homeserver(user, &user_id).await?;
+
+    // Add new node into the graph
+    user_details.put_to_graph().await?;
+
+    // Reindex to sorted sets and other indexes
+    reindex_user(&user_id).await?;
+    UserDetails::to_index(&[user_id.as_ref()], vec![Some(user_details)]).await?;
+
+    Ok(())
+}
+
+pub async fn handle_user_del_event(user_id: PubkyId) -> Result<(), Box<dyn Error + Sync + Send>> {
+    debug!("Deleting user profile:  {}", user_id);
+
+    if let Some(user_details) = UserDetails::get_by_id(&user_id).await? {
+        user_details.delete().await?;
+        UserCounts::delete(&user_id).await?;
+    }
+
+    Ok(())
+}
