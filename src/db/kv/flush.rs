4df5fbf7c656b1d25d0f9345aa7ecb31504fd6a0
SHAcollision <cotyleron@proton.me>
2024-09-06 11:43:04 +0200

    Fix new clippy deprecation warning on 1.81

--- src/db/kv/flush.rs
+++ src/db/kv/flush.rs
@@ -3,6 +3,6 @@ use std::error::Error;
 
 pub async fn clear_redis() -> Result<(), Box<dyn Error + Send + Sync>> {
     let mut redis_conn = get_redis_conn().await?;
-    redis::cmd("FLUSHDB").query_async(&mut redis_conn).await?;
+    let _: () = redis::cmd("FLUSHDB").query_async(&mut redis_conn).await?;
     Ok(())
 }
