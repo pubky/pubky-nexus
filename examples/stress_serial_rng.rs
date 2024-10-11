ac53bb3f9b1f15edc7742ec65277d8ff21e7dba9
amir <amir.r.amirfatahi@gmail.com>
2024-10-11 16:33:52 -0400

    fix examples

--- examples/stress_serial_rng.rs
+++ examples/stress_serial_rng.rs
@@ -277,6 +277,7 @@ async fn create_posts(
             kind: PostKind::Short,
             parent: None,
             embed: None,
+            attachments: None,
         };
         let post_id = post.create_id();
         let post_url = format!("pubky://{}/pub/pubky.app/posts/{}", pk, post_id);
