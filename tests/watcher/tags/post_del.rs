60dca9ccbe3284d99891ac04c4f44a6f556c656d
tipogi <tipogi@pm.me>
2024-10-14 07:02:41 +0200

    linter warnings

--- tests/watcher/tags/post_del.rs
+++ tests/watcher/tags/post_del.rs
@@ -46,7 +46,7 @@ async fn test_homeserver_del_tag_post() -> Result<()> {
         kind: PubkyAppPost::default().kind,
         parent: None,
         embed: None,
-        attachments: None
+        attachments: None,
     };
     let post_id = test.create_post(&author_user_id, &post).await?;
 
