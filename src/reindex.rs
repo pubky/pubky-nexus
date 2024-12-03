e3f19c4e5e1421b2cbe1fb252ac31b2a8eab2369
amir <amir.r.amirfatahi@gmail.com>
2024-12-03 01:38:40 -0500

    Add reindex

--- src/reindex.rs
+++ src/reindex.rs
@@ -7,7 +7,7 @@ use crate::models::tag::stream::HotTags;
 use crate::models::tag::traits::TagCollection;
 use crate::models::tag::user::TagUser;
 use crate::models::traits::Collection;
-use crate::models::user::{Muted, UserDetails};
+use crate::models::user::{Influencers, Muted, UserDetails};
 use crate::types::DynError;
 use crate::{
     db::connectors::neo4j::get_neo4j_graph,
@@ -68,6 +68,10 @@ pub async fn reindex() {
     HotTags::reindex()
         .await
         .expect("Failed to store the global hot tags");
+
+    Influencers::reindex()
+        .await
+        .expect("Failed to reindex influencers");
 
     TagSearch::reindex()
         .await
