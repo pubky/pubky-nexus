d01a7f2c2a4ef2f0e7b3b9ff18609f688e17b9c8
nazeh <ar.nazeh@gmail.com>
2025-01-06 14:02:53 +0300

    --ammend

--- tests/mainline.rs
+++ tests/mainline.rs
@@ -3,12 +3,13 @@ use mainline::Testnet;
 use std::time::Duration;
 
 #[tokio::test]
-async fn test_homeserver_post_attachments() -> Result<()> {
+async fn test_mainline() -> Result<()> {
     // let mut test = WatcherTest::setup().await?;
     env_logger::try_init()?;
 
-    let testnet = Testnet::new(100)?;
+    let testnet = Testnet::new(10)?;
 
     std::thread::sleep(Duration::from_secs(5));
-    panic!("{:?}", testnet);
+
+    Ok(())
 }
