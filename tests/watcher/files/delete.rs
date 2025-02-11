f0a1f186a5bebdabbd91e307fac5506bf355ace9
SHAcollision <cotyleron@proton.me>
2025-02-10 20:34:57 -0400

    Fix delete test

--- tests/watcher/files/delete.rs
+++ tests/watcher/files/delete.rs
@@ -3,9 +3,9 @@ use crate::{
 };
 use anyhow::Result;
 use chrono::Utc;
-use pubky_app_specs::{PubkyAppFile, PubkyAppUser};
+use pubky_app_specs::traits::HasPath;
+use pubky_app_specs::{PubkyAppBlob, PubkyAppFile, PubkyAppUser};
 use pubky_common::crypto::Keypair;
-use pubky_common::timestamp::Timestamp;
 use pubky_nexus::{
     models::{file::FileDetails, traits::Collection},
     PubkyConnector,
@@ -28,13 +28,14 @@ async fn test_delete_pubkyapp_file() -> Result<()> {
 
     let user_id = test.create_user(&keypair, &user).await?;
 
-    let blob = "Hello World!";
-    let blob_id = Timestamp::now().to_string();
-    let blob_url = format!("pubky://{}/pub/pubky.app/blobs/{}", user_id, blob_id);
+    let blob_data = "Hello World!".to_string();
+    let blob = PubkyAppBlob::new(blob_data.as_bytes().to_vec());
+    let blob_url = format!("pubky://{}{}", user_id, blob.create_path());
+
     let pubky_client = PubkyConnector::get_pubky_client()?;
     pubky_client
         .put(blob_url.as_str())
-        .json(&blob)
+        .body(blob.0)
         .send()
         .await?;
 
