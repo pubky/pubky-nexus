e39ffea89b173c76386f4c8927b327d53a194531
tipogi <103417381+tipogi@users.noreply.github.com>
2025-03-31 21:26:23 +0200

    bug: add a custom limit for user reach in post streams (#403)

--- nexus-common/src/models/post/stream.rs
+++ nexus-common/src/models/post/stream.rs
@@ -323,21 +323,22 @@ impl PostStream {
         skip: Option<usize>,
         limit: Option<usize>,
     ) -> Result<Vec<String>, DynError> {
+        let custom_limit = Some(200);
         let mut user_ids = match &source {
             StreamSource::Following { observer_id } => {
-                Following::get_by_id(observer_id, None, None)
+                Following::get_by_id(observer_id, None, custom_limit)
                     .await?
                     .unwrap_or_default()
                     .0
             }
             StreamSource::Followers { observer_id } => {
-                Followers::get_by_id(observer_id, None, None)
+                Followers::get_by_id(observer_id, None, custom_limit)
                     .await?
                     .unwrap_or_default()
                     .0
             }
             StreamSource::Friends { observer_id } => {
-                Friends::get_by_id(observer_id, None, None)
+                Friends::get_by_id(observer_id, None, custom_limit)
                     .await?
                     .unwrap_or_default()
                     .0
