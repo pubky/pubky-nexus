9141ce17fa603997213aa53ab266c816764d276c
ok300 <106775972+ok300@users.noreply.github.com>
2025-07-11 11:29:41 +0200

    DaemonLauncher: use try_join to detect failing starts

--- nexusd/src/launcher.rs
+++ nexusd/src/launcher.rs
@@ -4,7 +4,7 @@ use nexus_watcher::NexusWatcherBuilder;
 use nexus_webapi::NexusApiBuilder;
 use serde::{Deserialize, Serialize};
 use std::{fmt::Debug, path::PathBuf};
-use tokio::join;
+use tokio::try_join;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct DaemonLauncher {}
@@ -14,7 +14,8 @@ impl DaemonLauncher {
         let config = DaemonConfig::read_or_create_config_file(config_dir).await?;
         let nexus_webapi_builder = NexusApiBuilder::with_stack(config.api, &config.stack);
         let nexus_watcher_builder = NexusWatcherBuilder::with_stack(config.watcher, &config.stack);
-        let _ = join!(nexus_webapi_builder.start(), nexus_watcher_builder.start());
+
+        try_join!(nexus_webapi_builder.start(), nexus_watcher_builder.start())?;
         Ok(())
     }
 }
