120880a0ea4c43bad768f9328684f6c7a60575c8
ok300 <106775972+ok300@users.noreply.github.com>
2025-07-25 05:32:17 +0200

    Correctly handle tags containing emojis in tag search (#509)
    * Correctly handle unicode chars in tag search
    
    * Tag tests: extract and re-use JSON value parsing

--- nexus-webapi/tests/tags/search.rs
+++ nexus-webapi/tests/tags/search.rs
@@ -7,20 +7,23 @@ pub fn format_search_tags_by_prefix(prefix: &str) -> String {
     SEARCH_TAGS_BY_PREFIX_ROUTE.replace("{prefix}", prefix)
 }
 
+fn extract_str_vec(res: serde_json::Value) -> Vec<String> {
+    assert!(res.is_array());
+
+    res.as_array()
+        .expect("Tag search results should be an array")
+        .iter()
+        .map(|tag| tag.as_str().expect("Tag should be a string").to_string())
+        .collect()
+}
+
 #[tokio_shared_rt::test(shared)]
 async fn test_search_tags_by_prefix() -> Result<()> {
     let label_prefix = "he";
     let url_path = format_search_tags_by_prefix(label_prefix);
     let res = get_request(&url_path).await?;
 
-    assert!(res.is_array());
-
-    let fetched_tags: Vec<String> = res
-        .as_array()
-        .expect("Tag search results should be an array")
-        .iter()
-        .map(|tag| tag.as_str().expect("Tag should be a string").to_string())
-        .collect();
+    let fetched_tags: Vec<String> = extract_str_vec(res);
 
     let expected_tags = vec!["healthily", "heavily", "hello"];
 
@@ -35,16 +38,24 @@ async fn test_search_tags_by_prefix_sanitized() -> Result<()> {
     let url_path = format_search_tags_by_prefix(label_prefix);
     let res = get_request(&url_path).await?;
 
-    assert!(res.is_array());
-
-    let fetched_tags: Vec<String> = res
-        .as_array()
-        .expect("Tag search results should be an array")
-        .iter()
-        .map(|tag| tag.as_str().expect("Tag should be a string").to_string())
-        .collect();
+    let fetched_tags: Vec<String> = extract_str_vec(res);
 
     let expected_tags = vec!["healthily", "heavily", "hello"];
+
+    assert_eq!(fetched_tags, expected_tags);
+
+    Ok(())
+}
+
+#[tokio_shared_rt::test(shared)]
+async fn test_search_tags_with_emojis_by_prefix() -> Result<()> {
+    let label_prefix = "⭐⭐⭐";
+    let url_path = format_search_tags_by_prefix(label_prefix);
+    let res = get_request(&url_path).await?;
+
+    let fetched_tags: Vec<String> = extract_str_vec(res);
+
+    let expected_tags = vec!["⭐⭐⭐", "⭐⭐⭐⭐", "⭐⭐⭐⭐⭐"];
 
     assert_eq!(fetched_tags, expected_tags);
 
