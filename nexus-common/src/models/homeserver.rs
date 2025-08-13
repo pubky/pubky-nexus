49ff8f3f63bc8b6f8eab206defdda8fa635092e8
ok300 <106775972+ok300@users.noreply.github.com>
2025-08-13 19:16:33 +0200

    Add Homeserver::get_from_graph

--- nexus-common/src/models/homeserver.rs
+++ nexus-common/src/models/homeserver.rs
@@ -1,4 +1,5 @@
 use crate::db::execute_graph_operation;
+use crate::db::get_neo4j_graph;
 use crate::db::queries;
 use crate::db::OperationOutcome;
 use crate::db::RedisOps;
@@ -33,12 +34,34 @@ impl Homeserver {
         execute_graph_operation(query).await
     }
 
+    /// Retrieves a homeserver from Neo4j.
+    pub async fn get_from_graph(id: &str) -> Result<Option<Homeserver>, DynError> {
+        let mut result;
+        {
+            let graph = get_neo4j_graph()?;
+            let query = queries::get::get_homeserver_by_id(id);
+
+            let graph = graph.lock().await;
+            result = graph.execute(query).await?;
+        }
+
+        if let Some(row) = result.next().await? {
+            let hs = Self {
+                id: row.get("id").unwrap_or_default(),
+                cursor: "0000000000000".to_string(), // TODO Should cursor also be stored in graph? If so, also updated? When?
+            };
+            Ok(Some(hs))
+        } else {
+            Ok(None)
+        }
+    }
+
     /// Retrieves the homeserver from Redis.
     pub async fn get_from_index(id: &str) -> Result<Option<Self>, DynError> {
         Self::try_from_index_json(&[id], None).await
     }
 
-    /// Stores the homeserver in Redis.
+    /// Stores this homeserver in Redis.
     pub async fn put_to_index(&self) -> Result<(), DynError> {
         self.put_index_json(&[&self.id], None, None).await
     }
@@ -52,5 +75,57 @@ impl Homeserver {
                 Homeserver::new(homeserver).await
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use pubky::Keypair;
+    use pubky_app_specs::PubkyId;
+
+    use crate::types::DynError;
+
+    use super::*;
+
+    #[tokio_shared_rt::test(shared)]
+    async fn test_put_to_get_from_graph() -> Result<(), DynError> {
+        let keys = Keypair::random();
+        let id = PubkyId::try_from(&keys.public_key().to_z32())?;
+
+        let hs = Homeserver::new(id.clone());
+        hs.put_to_graph()
+            .await
+            .expect("Failed to put homeserver to graph");
+
+        let hs_from_graph = Homeserver::get_from_graph(&id)
+            .await
+            .ok()
+            .flatten()
+            .expect("Failed to get homeserver from graph");
+
+        assert_eq!(id, hs_from_graph.id);
+
+        Ok(())
+    }
+
+    #[tokio_shared_rt::test(shared)]
+    async fn test_put_to_get_from_index() -> Result<(), DynError> {
+        let keys = Keypair::random();
+        let id = PubkyId::try_from(&keys.public_key().to_z32())?;
+
+        let hs = Homeserver::new(id.clone());
+        hs.put_to_index()
+            .await
+            .expect("Failed to put homeserver to index");
+
+        let hs_from_index = Homeserver::get_from_index(&id)
+            .await
+            .ok()
+            .flatten()
+            .expect("Failed to get homeserver from index");
+
+        assert_eq!(id, hs_from_index.id);
+
+        Ok(())
     }
 }
