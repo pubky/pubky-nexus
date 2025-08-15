20c2675d777baf4b23285038eb98e10c3fdb993d
ok300 <106775972+ok300@users.noreply.github.com>
2025-08-15 07:38:41 +0200

    Fix usage of fetch_row_from_graph in user test utils

--- nexus-watcher/tests/users/utils.rs
+++ nexus-watcher/tests/users/utils.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use nexus_common::{
-    db::{queries, retrieve_from_graph, RedisOps},
+    db::{fetch_row_from_graph, queries, RedisOps},
     models::user::{
         UserCounts, UserDetails, UserStream, USER_INFLUENCERS_KEY_PARTS,
         USER_MOSTFOLLOWED_KEY_PARTS,
@@ -32,9 +32,12 @@ pub async fn find_user_counts(user_id: &str) -> UserCounts {
 
 pub async fn find_user_details(user_id: &str) -> Result<UserDetails> {
     let query = queries::get::get_users_details_by_ids(&[user_id]);
-    let maybe_record = retrieve_from_graph(query, "record").await.unwrap();
 
-    if let Some(result) = maybe_record {
+    // We always expect a row, even if no UserDetails are found
+    // The row contains: id (user_id), record (optional UserDetails)
+    let row = fetch_row_from_graph(query).await.unwrap().unwrap();
+
+    if let Ok(result) = row.get::<UserDetails>("record") {
         return Ok(result);
     }
     anyhow::bail!("User node not found in Nexus graph");
