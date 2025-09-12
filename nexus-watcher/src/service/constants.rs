847a595f4d6948f323880bb41b2def2bfef16a72
ok300 <106775972+ok300@users.noreply.github.com>
2025-09-12 15:50:27 +0200

    Remove unused MAX_CONCURRENT const (#566)

--- nexus-watcher/src/service/constants.rs
+++ nexus-watcher/src/service/constants.rs
@@ -1,7 +1,4 @@
 /// Name of the watcher config file
 pub const WATCHER_CONFIG_FILE_NAME: &str = "watcher-config.toml";
-// TODO: Maybe that ones should be configurable? .toml file?
-/// Max homeservers processed concurrently within a cycle
-pub const MAX_CONCURRENT: usize = 3;
 ///  Per-homeserver hard timeout (seconds)
 pub const PROCESSING_TIMEOUT_SECS: u64 = 3_600;
