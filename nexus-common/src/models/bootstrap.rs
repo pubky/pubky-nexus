a0c5f8a5f3b48215a349c21df80a2ec54519e4c6
aintnostressin <aintnostressin@proton.me>
2026-02-16 20:44:21 +0100

    add notifications to bootstrap

--- nexus-common/src/models/bootstrap.rs
+++ nexus-common/src/models/bootstrap.rs
@@ -1,6 +1,7 @@
 use std::collections::HashSet;
 
 use crate::db::kv::SortOrder;
+use crate::models::notification::Notification;
 use crate::models::tag::stream::{HotTag, HotTags};
 use crate::models::user::Muted;
 use crate::types::routes::HotTagsInputDTO;
@@ -32,6 +33,8 @@ pub struct Bootstrap {
     pub ids: BootstrapIds,
     /// Whether or not this user is already indexed
     pub indexed: bool,
+    /// Latest notifications
+    pub notifications: Vec<Notification>,
 }
 
 /// IDs of objects relevant to the bootstrap payload, for example
@@ -58,9 +61,9 @@ impl Bootstrap {
     /// Returns a populated response even if the user is not found or not indexed.
     ///
     /// # Parameters
-    /// - `user_id: &str`  
+    /// - `user_id: &str`
     ///   The ID of the user whose “ImAlive” stream is being built
-    /// - `view_type: ViewType`  
+    /// - `view_type: ViewType`
     ///   Controls whether to fetch replies and include full stream entries (`Full`)
     ///   or only base posts (`Partial`)
     pub async fn get_by_id(user_id: &str, view_type: ViewType) -> Result<Self, DynError> {
@@ -112,6 +115,9 @@ impl Bootstrap {
                 .get_and_merge_users(&missing_taggers, maybe_viewer_id)
                 .await?;
         }
+
+        // Add user's notifications
+        bootstrap.add_notifications(maybe_viewer_id).await?;
 
         // Return only ids in case of muted
         bootstrap.add_muted(maybe_viewer_id).await?;
@@ -180,9 +186,9 @@ impl Bootstrap {
     /// Appends each tagger’s user ID from the given post tag details into the provided set
     ///
     /// # Parameters
-    /// - `tag_details_list: &Vec<TagDetails>`  
+    /// - `tag_details_list: &Vec<TagDetails>`
     ///   A reference to a vector of `TagDetails`, each containing a list of tagger IDs
-    /// - `users_list: &mut HashSet<String>`  
+    /// - `users_list: &mut HashSet<String>`
     ///   A mutable reference to a set of user IDs; each tagger ID will be inserted here
     fn insert_taggers_id(tag_details_list: &[TagDetails], users_list: &mut HashSet<String>) {
         for tag_details in tag_details_list.iter() {
@@ -195,9 +201,9 @@ impl Bootstrap {
     /// Fetches and appends user views for the given set of `user_ids`
     ///
     /// # Parameters
-    /// - `user_ids: HashSet<String>`  
+    /// - `user_ids: HashSet<String>`
     ///   A set of unique user IDs to fetch views for
-    /// - `viewer_id: Option<&str>`  
+    /// - `viewer_id: Option<&str>`
     ///   Optional context user ID for personalized view generation
     async fn get_and_merge_users(
         &mut self,
@@ -222,11 +228,11 @@ impl Bootstrap {
     /// into both the internal user list
     ///
     /// # Parameters
-    /// - `post_replies: Vec<(String, String)>`  
+    /// - `post_replies: Vec<(String, String)>`
     ///   A list of `(author_id, post_id)` tuples indicating which post replies to fetch
-    /// - `user_ids: &mut HashSet<String>`  
+    /// - `user_ids: &mut HashSet<String>`
     ///   A mutable reference to a set where each reply’s author ID (and any taggers) will be appended
-    /// - `maybe_viewer_id: Option<&str>`  
+    /// - `maybe_viewer_id: Option<&str>`
     ///   The ID of the current viewer
     async fn get_and_handle_replies(
         &mut self,
@@ -251,11 +257,11 @@ impl Bootstrap {
     /// Fetches a post stream timeline for the given `source` and `limit`
     ///
     /// # Parameters
-    /// - `maybe_viewer_id: Option<&str>`  
+    /// - `maybe_viewer_id: Option<&str>`
     ///   Optional context user ID for personalized view generation
-    /// - `source: StreamSource`  
+    /// - `source: StreamSource`
     ///   The source of the post stream
-    /// - `limit: usize`  
+    /// - `limit: usize`
     ///   The limit of the post stream
     async fn get_post_stream_timeline(
         maybe_viewer_id: Option<&str>,
@@ -294,6 +300,13 @@ impl Bootstrap {
                 self.ids.influencers.push(id.clone());
                 user_ids.insert(id);
             });
+        }
+        Ok(())
+    }
+
+    async fn add_notifications(&mut self, maybe_viewer_id: Option<&str>) -> Result<(), DynError> {
+        if let Some(viewer_id) = maybe_viewer_id {
+            self.notifications = Notification::get_by_id(viewer_id, Default::default()).await?;
         }
         Ok(())
     }
