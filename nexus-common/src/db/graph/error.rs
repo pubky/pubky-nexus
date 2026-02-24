46172f1a44e60431eb9a6bfbc109a258cfeebcf5
ok300 <106775972+ok300@users.noreply.github.com>
2026-02-24 07:15:41 +0100

    Simplify GraphError

--- nexus-common/src/db/graph/error.rs
+++ nexus-common/src/db/graph/error.rs
@@ -8,32 +8,20 @@ pub type GraphResult<T> = std::result::Result<T, GraphError>;
 pub enum GraphError {
     #[error("Connection not initialized")]
     ConnectionNotInitialized,
-    #[error("Query failed")]
-    QueryFailed(#[source] DynError),
+    #[error("Query failed: {0}")]
+    QueryFailed(#[from] neo4rs::Error),
     #[error("Query timeout")]
     QueryTimeout,
     #[error("Query build error: {0}")]
     QueryBuildError(String),
     #[error("Serialization failed")]
     SerializationFailed(#[source] DynError),
-    #[error("Deserialization failed")]
-    DeserializationFailed(#[source] DynError),
+    #[error("Deserialization failed: {0}")]
+    DeserializationFailed(#[from] neo4rs::DeError),
     #[error("URI parse error: {0}")]
     UriParseError(String),
     #[error("Invalid resource type: {0}")]
     InvalidResourceType(String),
     #[error("Generic: {0}")]
     Generic(String),
-}
-
-impl From<neo4rs::Error> for GraphError {
-    fn from(e: neo4rs::Error) -> Self {
-        GraphError::QueryFailed(e.into())
-    }
-}
-
-impl From<neo4rs::DeError> for GraphError {
-    fn from(e: neo4rs::DeError) -> Self {
-        GraphError::DeserializationFailed(Box::new(e))
-    }
 }
