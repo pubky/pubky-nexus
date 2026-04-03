c01365dbb340e271f3b58f2e28ede9da8ffbd860
ok300 <106775972+ok300@users.noreply.github.com>
2026-04-03 14:22:34 +0200

    chore: remove TODO

--- nexus-watcher/src/service/user_hs_resolver.rs
+++ nexus-watcher/src/service/user_hs_resolver.rs
@@ -62,7 +62,6 @@ async fn resolve_user(user_id: &str) -> Result<(), DynError> {
 }
 
 /// Returns all user IDs hosted on a given homeserver.
-/// TODO Should be used in [EventProcessor::poll_events]
 pub async fn get_user_ids_by_homeserver(hs_id: &str) -> GraphResult<Vec<String>> {
     let query = queries::get::get_users_by_homeserver(hs_id);
     let maybe_user_ids = fetch_key_from_graph(query, "user_ids").await?;
