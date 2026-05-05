eddcacfcfa97c1b1612d5969e75529601ee275d9
aintnostressin <aintnostressin@proton.me>
2026-05-05 13:16:36 +0200

    extract non existing user id

--- nexus-webapi/tests/user/reach.rs
+++ nexus-webapi/tests/user/reach.rs
@@ -2,6 +2,8 @@ use crate::utils::{get_request, invalid_get_request};
 use anyhow::Result;
 use axum::http::StatusCode;
 
+const NON_EXISTING_USER_ID: &str = "4snwyct86m383rsduhw5xgcxpw7c63j3pq8x4ycqikxgik8y64rp";
+
 #[tokio_shared_rt::test(shared)]
 async fn test_get_followers() -> Result<()> {
     let user_id = "4snwyct86m383rsduhw5xgcxpw7c63j3pq8x4ycqikxgik8y64ro";
@@ -43,10 +45,7 @@ async fn test_get_followers() -> Result<()> {
 
     // Test non-existing user
     invalid_get_request(
-        &format!(
-            "/v0/user/{}/followers",
-            "4snwyct86m383rsduhw5xgcxpw7c63j3pq8x4ycqikxgik8y64rp"
-        ),
+        &format!("/v0/user/{NON_EXISTING_USER_ID}/followers"),
         StatusCode::NOT_FOUND,
     )
     .await?;
@@ -100,10 +99,7 @@ async fn test_get_following() -> Result<()> {
 
     // Test non-existing user
     invalid_get_request(
-        &format!(
-            "/v0/user/{}/following",
-            "4snwyct86m383rsduhw5xgcxpw7c63j3pq8x4ycqikxgik8y64rp"
-        ),
+        &format!("/v0/user/{NON_EXISTING_USER_ID}/following"),
         StatusCode::NOT_FOUND,
     )
     .await?;
@@ -150,10 +146,7 @@ async fn test_get_friends() -> Result<()> {
 
     // Test non-existing user
     invalid_get_request(
-        &format!(
-            "/v0/user/{}/friends",
-            "4snwyct86m383rsduhw5xgcxpw7c63j3pq8x4ycqikxgik8y64rp"
-        ),
+        &format!("/v0/user/{NON_EXISTING_USER_ID}/friends"),
         StatusCode::NOT_FOUND,
     )
     .await?;
