6556f617965511b63fbfce18d086558cc27ec2f5
aintnostressin <aintnostressin@proton.me>
2026-05-12 08:19:57 +0200

    Revert "remove duplicated path"
    This reverts commit 63bfb89132db9ea0c3a71c86341246c09645c7ba.

--- nexus-watcher/tests/event_processor/files/delete.rs
+++ nexus-watcher/tests/event_processor/files/delete.rs
@@ -10,7 +10,6 @@ use pubky_app_specs::{
     traits::{HasIdPath, HashId},
     PubkyAppBlob, PubkyAppFile, PubkyAppUser, PubkyId,
 };
-use std::path::Path;
 
 #[tokio_shared_rt::test(shared)]
 async fn test_delete_pubkyapp_file() -> Result<()> {
@@ -50,10 +49,10 @@ async fn test_delete_pubkyapp_file() -> Result<()> {
     let (file_id, file_path) = test.create_file(&user_kp, &file).await?;
 
     let result_file = assert_file_details(&user_id, &file_id, &blob_absolute_url, &file).await;
-    let file_path_on_disk = result_file.urls.main.clone();
 
+    let blob_static_path = test.temp_dir.path().join(&result_file.urls.main);
     assert!(
-        Path::new(&file_path_on_disk).exists(),
+        blob_static_path.exists(),
         "File have to exist after PUT event"
     );
 
@@ -79,8 +78,14 @@ async fn test_delete_pubkyapp_file() -> Result<()> {
     assert!(result_file.is_none());
 
     // Assert: Ensure it's deleted
+    let blob_static_path = test
+        .temp_dir
+        .path()
+        .join(&user_id)
+        .join(&file_id)
+        .join("main");
     assert!(
-        !Path::new(&file_path_on_disk).exists(),
+        !blob_static_path.exists(),
         "File cannot exist after DEL event"
     );
 
