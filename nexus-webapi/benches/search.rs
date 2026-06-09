0982e297c1378b22d49e6ce0fb93dd1f61c36c80
aintnostressin <aintnostressin@proton.me>
2026-06-09 16:39:26 +0200

    chore: add FTS benches

--- nexus-webapi/benches/search.rs
+++ nexus-webapi/benches/search.rs
@@ -1,6 +1,10 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use nexus_common::{
-    models::{post::search::PostsByTagSearch, tag::search::TagSearch, user::UserSearch},
+    models::{
+        post::search::{PostsByContentSearch, PostsByTagSearch},
+        tag::search::TagSearch,
+        user::UserSearch,
+    },
     types::Pagination,
 };
 use setup::run_setup;
@@ -86,6 +90,28 @@ fn bench_post_tag_search_by_timeline(c: &mut Criterion) {
     );
 }
 
+fn bench_post_content_search(c: &mut Criterion) {
+    println!("******************************************************************************");
+    println!("Benchmarking post content search.");
+    println!("******************************************************************************");
+
+    run_setup();
+
+    let query = "free";
+    let rt = Runtime::new().unwrap();
+
+    c.bench_with_input(
+        BenchmarkId::new("post_content_search", query),
+        &query,
+        |b, &query| {
+            b.to_async(&rt).iter(|| async {
+                let result = PostsByContentSearch::search(query, 0, 20).await.unwrap();
+                std::hint::black_box(result);
+            });
+        },
+    );
+}
+
 fn configure_criterion() -> Criterion {
     Criterion::default()
         .measurement_time(Duration::new(5, 0))
@@ -98,7 +124,8 @@ criterion_group! {
     config = configure_criterion();
     targets =   bench_user_search,
                 bench_tag_search,
-                bench_post_tag_search_by_timeline
+                bench_post_tag_search_by_timeline,
+                bench_post_content_search
 }
 
 criterion_main!(benches);
